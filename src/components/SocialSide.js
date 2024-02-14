97852fa2d9820179975a3e1e94c939e740775d98
jahanzaibali9211 <m.jahanzaiba.k@gmail.com>
2024-02-14 22:17:27 +0500

    changes

--- src/components/SocialSide.js
+++ src/components/SocialSide.js
@@ -3,10 +3,10 @@ import React from 'react';
 const SocialSide = () => {
     return (
         <>
-            <div class="sticky-icon">
-                <a href="https://www.linkedin.com/in/jahanzaib-ali-084101235/" target='_blank' rel="noreferrer" class="Linkedin"><i class="fab fa-linkedin-in"></i> Linkedin </a>
-                <a href="https://github.com/jahanzaibali9211/" target='_blank' rel="noreferrer" class="Github"><i class="fab fa-github"></i> Github </a>
-                <a href="https://web.whatsapp.com/send?phone=+923012571742&text=" target='_blank' rel="noreferrer" class="Whatsapp"><i class="fab fa-whatsapp"></i> Whatsapp </a>
+            <div className="sticky-icon">
+                <a href="https://www.linkedin.com/in/jahanzaib-ali-084101235/" target='_blank' rel="noreferrer" className="Linkedin"><i className="fab fa-linkedin-in"></i> Linkedin </a>
+                <a href="https://github.com/jahanzaibali9211/" target='_blank' rel="noreferrer" className="Github"><i className="fab fa-github"></i> Github </a>
+                <a href="https://web.whatsapp.com/send?phone=+923012571742&text=" target='_blank' rel="noreferrer" className="Whatsapp"><i className="fab fa-whatsapp"></i> Whatsapp </a>
             </div>
         </>
     );
